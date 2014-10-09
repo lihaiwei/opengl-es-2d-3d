f6a4ef0ca7cb7e3fed3176ed74b8c35cf57b4db3
BobLChen <chenbo150928@mail.com>
2014-10-09 18:38:01 +0800

    update

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,16 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 
+
+# monkey模块
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := monkey
 
-LOCAL_SRC_FILES := jnihelper.c
-				   
+JNI_SOURCES := jnihelper.c
+
+LOCAL_SRC_FILES += ${JNI_SOURCES}
+
 LOCAL_LDLIBS := -lGLESv1_CM \
                 -lGLESv2 \
                 -lEGL \
                 -llog \
                 -lz \
                 -landroid
-
+                
 include $(BUILD_SHARED_LIBRARY)
+
+
+
+
+
+
+
